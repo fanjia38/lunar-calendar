daadcbfc73d2ccb454f627ca6602d5fd2fa174b2
fanjia38 <38fanjia@gmail.com>
2021-02-08 00:26:10 +0900

    :tada: Next.js で再実装

---
+++ components/content/hooks.ts
@@ -0,0 +1,42 @@
+import { useState, useMemo } from 'react'
+import solarLunar from 'solarLunar'
+import { format, addYears, getYear, getMonth, getDate } from 'date-fns'
+
+const useCalendar = () => {
+    const [selectDate, setSelectDate] = useState(format(addYears(new Date(), -20), "yyyy-MM-dd"))
+
+    const lunarDateObject = useMemo(() => {
+        const dateTime = new Date(selectDate)
+
+        const year = getYear(dateTime)
+        const month = getMonth(dateTime) + 1
+        const date = getDate(dateTime)
+
+        return solarLunar.solar2lunar(year, month, date)
+    }, [selectDate])
+
+    const solarDateObject = useMemo(() => {
+        return solarLunar.lunar2solar(getYear(new Date()), lunarDateObject.lMonth, lunarDateObject.lDay);
+    }, [lunarDateObject])
+
+    const lunarMessage = useMemo(() => {
+        return `${lunarDateObject.cYear}年${lunarDateObject.cMonth}月${lunarDateObject.cDay}日は、旧暦 ${lunarDateObject.lYear}年${lunarDateObject.lMonth}月${lunarDateObject.lDay}日 です。`
+    }, [lunarDateObject])
+
+    const solarMessage = useMemo(() => {
+        return `今年の旧暦${solarDateObject.lMonth}月${solarDateObject.lDay}は、${solarDateObject.cYear}年${solarDateObject.cMonth}月${solarDateObject.cDay}日です。`
+    }, [solarDateObject])
+
+    console.log(lunarDateObject)
+    console.log(solarDateObject)
+
+    return {
+        selectDate,
+        lunarMessage,
+        solarMessage,
+        setSelectDate
+    }
+}
+export {
+    useCalendar
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Box, Container, Heading, Text } from "@chakra-ui/react"
+import Content from '../components/content'
+
 
 const Index = () => {
     return (
-        <p>test</p>
+        <Box height="100vh" backgroundColor="#caaca7" color="#364f53">
+            <Box backgroundColor="#364f53" color="#caaca7" p="4">
+                <Heading>Lunar Calendar</Heading>
+                <Text>旧歴の日付を調べる</Text>
+            </Box>
+            <Content />
+            <Container marginTop="8" p="4" border="1px solid #364f53" borderRadius="8px">
+                <Text fontSize="sm">
+                    これは、中国の太陰太陽暦を計算するためのライブラリを使用しているため、日本の天保暦や新暦旧暦の対照表とは異なる結果が表示される可能性があります。
+                </Text>
+            </Container>
+        </Box>
     )
 }
 export default Index
